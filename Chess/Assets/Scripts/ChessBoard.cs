cbcab65e282cd52d31dd6ae08b7ddc8192d5c5b0
LAPTOP-9TJ201QT\anvit <anvit02surve@gmail.com>
2022-01-14 13:05:41 +0530

    Generate Board

---
+++ Chess/Assets/Scripts/ChessBoard.cs
@@ -0,0 +1,51 @@
+using System;
+using UnityEngine;
+
+public class ChessBoard : MonoBehaviour
+{
+    [Header("Art Stuff")]
+    [SerializeField] private Material tileMaterial;
+
+    //LOGIC
+    private const int tileCount_X = 8;
+    private const int tileCount_Y = 8;
+    private GameObject[,] tiles;
+
+    private void Awake() {
+        generateTiles(1, tileCount_X, tileCount_Y);
+    }
+    
+    //Generate Board
+    private void generateTiles(float tileSize, int tileCountX, int tileCountY){
+        tiles = new GameObject[tileCountX, tileCountY];
+        for (int x = 0; x < tileCountX; x++)
+            for (int y = 0; y < tileCountY; y++)
+                tiles[x,y] = generateSingleTile(tileSize, x, y);
+    }
+    private GameObject generateSingleTile(float tileSize, int x, int y)
+    {
+        GameObject tileObject = new GameObject(string.Format("X:{0}, Y:{1}", x, y));
+        tileObject.transform.parent = transform;
+
+        Mesh mesh = new Mesh();
+        tileObject.AddComponent<MeshFilter>().mesh = mesh;
+        tileObject.AddComponent<MeshRenderer>().material = tileMaterial;
+
+        Vector3[] vertices = new Vector3[4];
+        vertices[0] = new Vector3(x * tileSize, 0, y * tileSize);
+        vertices[1] = new Vector3(x * tileSize, 0, (y+1) * tileSize);
+        vertices[2] = new Vector3((x+1) * tileSize, 0, y * tileSize);
+        vertices[3] = new Vector3((x+1) * tileSize, 0, (y+1) * tileSize);
+
+        int[] tris = new int[] {0, 1, 2, 1, 3, 2};
+
+        mesh.vertices = vertices;
+        mesh.triangles = tris;
+
+        mesh.RecalculateNormals();
+
+        tileObject.AddComponent<BoxCollider>();
+
+        return tileObject;
+    }
+}
